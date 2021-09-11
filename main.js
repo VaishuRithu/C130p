da5d42f19b80a89b8554b4fe7d1753aec03d71b8
VaishuRithu <73801663+VaishuRithu@users.noreply.github.com>
2021-09-11 10:45:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,97 @@
+ song1 = "";
+ song2 = "";
+ leftWristX = 0;
+ leftWristY = 0;
+ rightWristX = 0;
+ rightWristY = 0;
+ scoreLeftWrist = 0;
+ scoreRightWrist = 0;
+ song1_status = "";
+ song2_status = "";
+
+ function preload(){
+    song1 = loadSound("invisible.mp3");
+    song2 = loadSound("dontletmedown.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreRightWrist > 0.2)
+    {
+        circle(rightWristX, rightWristY, 20);
+
+        song2.stop();
+
+        if(song1_status == false){
+            
+            song1.play();
+
+            document.getElementById("song-name").innerHTML = "Invisible By Julius Dreisig and Zeus X Crona";
+
+        }
+    }
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+
+        song1.stop();
+
+        if(song2_status == false){
+            
+            song2.play();
+
+            document.getElementById("song-name").innerHTML = "Don't Let Me Down by The Chainsmokers (Illenium Ver.)";
+
+        }
+    }
+}
+
+function gotPoses(results){
+    if (results.length > 0)
+    {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+        
+    }
+}
+
+function play()
+{
+   song.play();
+   song.setVolume(1);
+   song.rate(1);
+}
